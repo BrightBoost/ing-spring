9bd360631a038ba0b8f8cb0b91ee4e27f91aa935
brightboost <info@brightboost.nl>
2022-04-11 11:02:47 +0200

    day 5

---
+++ day5/jms-demo/src/main/java/com/examplejms/jmsdemo/Receiver.java
@@ -0,0 +1,13 @@
+package com.examplejms.jmsdemo;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Receiver {
+
+    @JmsListener(destination = "messageBox", containerFactory = "myFactory")
+    public void receiveMessage(Message message) {
+        System.out.println("Received: " + message);
+    }
+
+}
